16cfbf509eced4132f47b5a49fe0d259e94f0a65
Glitch (dereck-rock-paper) <none>
2021-12-15 23:28:19 +0000

    Day 4-5

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-let randomNumer = 0;
-
+let randomNumber = 0;
+let choices=["rock","paper","scissors"];
 $("#shoot").click(function() {
   let user = $("input").val();
   $("#userChoice").text(user);
   let randomNumber = Math.ceil(Math.random() * 3 )-1;
   $("#computerChoice").text(choices[randomNumber]);
+    console.log(choices[20]);
+
+    let computerChoice="No choice.";
+    if(randomNumber<=1){
+      computerChoice="Rock";
+    } else if(randomNumber >1 && randomNumber <=2){
+      computerChoice="paper";
+    } else{
+      computerChoice="Scissors";
+    }
+    $(".computerChoice").text(computerChoice);
 });
-
-let choices=["rock","paper","scissors"];
-console.log(choices[20]);
-
 
 
 
